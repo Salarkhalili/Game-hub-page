8cbf2843ffef9fc89c4f8d62d31f7fc619f7dbfa
salar <salarkhalili2018@gmail.com>
2024-03-04 15:52:58 +0330

    Refactor: Extracting query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import { Genre } from "./useGenres";
 import { Platform } from "./usePlatform";
 import useData from "./usedata";
@@ -18,19 +19,16 @@ export interface platform {
   slug: string;
 }
 
-const useGames = (
-  selectedGenre: Genre | null,
-  selectedPlatform: Platform | null
-) =>
+const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
     {
       params: {
-        genres: selectedGenre?.id,
-        platforms: selectedPlatform?.id,
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
       },
     },
-    [selectedGenre?.id, selectedPlatform?.id]
+    [gameQuery]
   );
 
 export default useGames;
