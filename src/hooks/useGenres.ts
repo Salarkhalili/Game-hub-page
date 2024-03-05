745ac58bae9498f4593bd4b2359407a2c7fa2577
salar <salarkhalili2018@gmail.com>
2024-03-05 22:43:49 +0330

    Customize the theme to get darker grays

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,13 @@
-import useData from "./usedata";
+import genres from "../data/genres";
+// import useData from "./usedata";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenre = () => useData<Genre>("/genres");
+// const useGenre = () => useData<Genre>("/genres");
+//!part36  میخواهیم به جای اینکه لیست ژانرهارو از سرور بگیره یکبار ذخیره اش کنیم در برنامه خودمون و هر بار از روی اون و در لحظه بدون لودینگ بیارتشون روی صفحه پس به جای هوک از داده های دستی برای دیتا و ارور و ایزلودینگ استفاده میکنیم
+const useGenre = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenre;
--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,10 +1,12 @@
-import useData from "./usedata";
+import platforms from "../data/platforms";
+// import useData from "./usedata";
 
 export interface Platform {
   id: number;
   name: string;
   plug: string;
 }
-const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+// const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+const usePlatform = () => ({ data: platforms, error: null });
 
 export default usePlatform;
